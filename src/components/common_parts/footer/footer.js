b68ff7cc2d6b8970b06ab5cd99b699e2ae70b6fb
v-rogr <v-rogru@microsoft.com>
2019-11-06 14:59:52 -0800

    added footer

---
+++ src/components/common_parts/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer-container'>We're ridding the world of secrets</div>
+    </footer>
+  );
+};
+
+export default Footer;
